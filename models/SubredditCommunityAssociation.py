9908390f443a040aa4fd2468f98258a1c9a4a824
tom dunn <tidunn13@gmail.com>
2013-07-23 23:05:51 -0400

    removing old commented out code

--- models/SubredditCommunityAssociation.py
+++ models/SubredditCommunityAssociation.py
@@ -5,14 +5,3 @@ association_table = Table('sc_association', Base.metadata,
         Column('community_id', Integer, ForeignKey('communities.id')),
         Column('subreddit_id', String,  ForeignKey('subreddits.id'))
 )
-
-"""
-    class SubredditCommunityAssociation(Base):
-        __tablename__ = 'subredditcommunityassociations'
-
-        subreddit_id = Column(String, ForeignKey('subreddits.id'), primary_key = True)
-        community_id = Column(Integer, ForeignKey('communities.id'), primary_key = True)
-
-        community = relationship("Community")
-        subreddit = relationship("Subreddit")
-"""
