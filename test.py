5b4eaf42f0c41bc96b4bbc85e050e55c4fb94737
tom dunn <tidunn13@gmail.com>
2013-09-13 23:24:52 -0700

    ...

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 from db import Session
 from models.Subreddit import Subreddit as Sub
 from models.DiscoveredSub import DiscoveredSub as DSub
+from models.User import User
+from models.Post import Post
 from helpers.DBIterator import DBIterator as DBI
 
 s = Session()
 DI = DBI
 Sub = Sub
 DSub = DSub
+P = Post
+U = User
